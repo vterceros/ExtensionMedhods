8f9013b5ce9ca3326ca87e4f8b57b778f76c2d25
vterceros <vh.terceros@gmail.com>
2018-03-27 11:54:15 -0400

    Extension Methods

---
+++ MyExtension.Library/Operations_Extend.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThirdPart.Library;
+namespace MyExtension.Library
+{
+    public static class Operations_Extend
+    {
+        /// <summary>
+        /// This is an extension method for the Operations class, this can be used when the code is not available
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="X"></param>
+        /// <param name="Y"></param>
+        /// <param name="Z"></param>
+        /// <returns></returns>
+        public static int CalculateVolume(this Operations obj, int X, int Y, int Z)
+        {
+            return X * Y * Z;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThirdPart.Library;
+using MyExtension.Library;
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations ope = new Operations();
+            Console.WriteLine("Existing Method");
+            Console.WriteLine(ope.CalculateArea(2, 2));
+            Console.WriteLine("Extended Method");
+            Console.WriteLine(ope.CalculateVolume(2, 2, 2));
+            Console.ReadKey();
+        }
+    }
+}
