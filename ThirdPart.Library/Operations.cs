54e3d97507472609552455534c6aa0be8d8938b6
vterceros <vh.terceros@gmail.com>
2018-03-27 12:53:17 -0400

    vh

---
+++ ThirdPart.Library/Operations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdPart.Library
+{
+    public class Operations
+    {
+        /// <summary>
+        /// This is a Third party dll and it is supposed we don't have this Source Code in order to use extensions
+        /// </summary>
+        public Operations()
+        {
+
+        }
+        public int CalculateArea(int X, int Y)
+        {
+            return X * Y;
+        }
+    }
+}
